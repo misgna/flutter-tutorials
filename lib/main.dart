1b0d3f5d4b9f1f120be9ea9a5829db5baae3be27
= <haile.misgna@proton.me>
2023-01-13 17:47:52 +0300

    ExplorationApp

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Exploration',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Space Exploration Planner!')),
+      body: Column(
+        children: [
+          Progress(),
+          TaskList(),
+        ],
+      ),
+    );
+  }
+}
+
+class Progress extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("You are this far away in the whole universe"),
+        LinearProgressIndicator(value: 0.0)
+      ],
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TaskItem(label: "Load rocket with supplies"),
+        TaskItem(label: "Launch rocket"),
+        TaskItem(label: "Circle the home planet"),
+        TaskItem(label: "Head out to the first moon"),
+        TaskItem(label: "Launch moon lander #1"),
+      ],
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  final String label;
+
+  const TaskItem({Key? key, required this.label}) : super(key: key);
+
+  @override
+  _TaskItemState createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  bool? _value = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+            onChanged: (newValue) => setState(() => _value = newValue),
+            value: _value),
+        Text(widget.label)
+      ],
+    );
+  }
+}
